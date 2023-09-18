60feabe5cf9ff813d01d9a831c22f4e8467114eb
Prin Techarong <techarong.prin@gmail.com>
2023-09-18 14:06:56 +0700

    Updated

--- 01 Palindrome Checker.js
+++ 01 Palindrome Checker.js
@@ -1,12 +1,14 @@
 function palindrome(str) {
   let thaiString = str.replace(/\W+|_/g, "").toLowerCase();
   let reversedString = thaiString.split("").reverse().join("");
-//console.log(thaiString, reversedString);
+console.log(thaiString, reversedString);
+
   if (thaiString != reversedString) {
     return false;
   }
   return true;
 }
+
 /*Or (Line 5-8)
   return (thaiString != reversedString) ? false : true;*/
 
